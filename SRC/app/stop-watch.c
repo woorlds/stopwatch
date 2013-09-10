b2c18a1746174525d6ce94271d827d1ca07050a8
Hyunwoo Lee <woorlds@gmail.com>
2013-09-10 19:48:57 +0900

    Add stop-watch header

--- SRC/app/stop-watch.c
+++ SRC/app/stop-watch.c
@@ -10,95 +10,59 @@
  *
  * =====================================================================
  */
-#include "2440addr.h"
 
-#include "includes.h"
-#include "bicycle.h"
-
+#include "stop-watch.h"
 #include "common.h"
 
-/* ********************************************************************* */
-/* Global definitions */
 
-
-/* ********************************************************************* */
-/* File local definitions */
-
-#define  TASK_STK_SIZE 		512                 /* Size of each task's stacks (# of WORDs) */
-#define  NO_TASKS           10                  /* Number of identical tasks */
-#define  MAZE_SIZE  		19
-
-
-OS_STK   TaskStk[NO_TASKS][TASK_STK_SIZE];      /* Tasks stacks */
-OS_STK   TaskStartStk[TASK_STK_SIZE];
-char     TaskData[NO_TASKS];                    /* Parameters to pass to each task */
-
-
-/***************************************
- * 
- * Title: Timer_Interrupt
- * 
- ***************************************/
-
-void Timer0_ISR()
+void Main()
 {
-	/* TODO : Pending Register Clear */
+	// HW init
+	hardware_init();
+
+	// Clear screen to green
+	clear_screen(GREEN);
+
+	Uart_Send_String("Stop Watch in uC/OS-II. Designed by Hyunwoo Lee\n");
+	Lcd_Printf(10*8,0,WHITE,GREEN,1,1,"Stop Watch. Designed by Hyunwoo Lee");  
+
+	Timer_Init();
+
+    OSInit();                               /* Initialize uC/OS-II */
+    //RandomSem = OSSemCreate(1);             /* Random number semaphore */
+    OSTaskCreate(TaskStart, (void *)0, (void *)&TaskStartStk[TASK_STK_SIZE - 1], 0);
+
+    OSStart();                              /* Start multitasking */
+	
+	while(1);	
+}
+
+
+void Timer0_ISR(void)
+{
 	rSRCPND |= (1<<10);
 	rINTPND |= (1<<10);
 
-//	Uart_Send_String("T");
 	OSTimeTick();
-
 }
 
-void Inval_ISR()
+
+void Inval_ISR(void)
 {
 	Uart_Printf("Invalid Interrupt");
 }
 
-void (*irqtbl[32])(void)={
-	Inval_ISR,	//	EINT0
-	Inval_ISR,	//	EINT1
-	Inval_ISR,	//	EINT2
-	Inval_ISR,	//	EINT3
-	Inval_ISR,	//	EINT4_7
-	Inval_ISR,	//	EINT8_23
-	Inval_ISR,	//	pISR_CAM
-	Inval_ISR,	//	BAT_FLT	      
-	Inval_ISR,	//	TICK	      
-	Inval_ISR,	//	WDT_AC97T_AC97
-	Timer0_ISR,	//	TIMER0	      
-	Inval_ISR,	//	TIMER1	      
-	Inval_ISR,	//	TIMER2	      
-	Inval_ISR,	//	TIMER3	      
-	Inval_ISR,	//	TIMER4	      
-	Inval_ISR,	//      UART2	      
-    Inval_ISR,      //      LCD	      
-	Inval_ISR,      //      DMA0	      
-	Inval_ISR,	//	DMA1	 														     
-	Inval_ISR,	//	DMA2	 														     
-	Inval_ISR,	//	DMA3	 														     
-	Inval_ISR,	//	SDI	 														     
-	Inval_ISR,	//	SPI0	 														     
-	Inval_ISR,	//	UART1	 														     // Added for 2440.
-	Inval_ISR,	//	NFCON	 														     
-	Inval_ISR,	//	USBD	 														     
-	Inval_ISR,	//	USBH	 														     
-	Inval_ISR,	//	IIC	 														     
-	Inval_ISR,	//	UART0	 																     
-	Inval_ISR,	//	SPI1	 																     
-	Inval_ISR,	//	RTC	 																     
-	Inval_ISR	//	ADC	 																     
-};
 
 void C_IRQHandler(void)
 {
 	irqtbl[rINTOFFSET]();
 }
 
+
 void Task (void *data)
 {
 }
+
 
 void TaskStart (void *data)
 {
@@ -145,23 +109,4 @@ void TaskStart (void *data)
                 OSTimeDlyHMSM(0, 0, 1, 0);      /* Wait one second */
 
         }
-}
-
-void Main()
-{
-	hardware_init();
-	clear_screen(GREEN);
-
-	Uart_Send_String("Stop Watch in uC/OS-II. Designed by Hyunwoo Lee\n");
-	Lcd_Printf(10*8,0,WHITE,GREEN,1,1,"Stop Watch. Designed by Hyunwoo Lee");  
-
-	Timer_Init();
-
-    OSInit();                               /* Initialize uC/OS-II                                      */
-    //RandomSem = OSSemCreate(1);             /* Random number semaphore                              */
-    OSTaskCreate(TaskStart, (void *)0, (void *)&TaskStartStk[TASK_STK_SIZE - 1], 0);
-
-    OSStart();                              /* Start multitasking                                       */
-	
-	while(1);	
 }
---
+++ SRC/app/stop-watch.h
@@ -0,0 +1,62 @@
+#ifndef __STOPWATCH_H__
+#define __STOPWATCH_H__
+
+#include "2440addr.h"
+#include "includes.h"
+
+/* Size of each task's stacks (# of WORDs) */
+#define  TASK_STK_SIZE 		512    
+
+/* Number of identical tasks */             
+#define  NO_TASKS           10                  
+#define  MAZE_SIZE  		19
+
+ /* Tasks stacks */
+OS_STK   TaskStk[NO_TASKS][TASK_STK_SIZE];     
+OS_STK   TaskStartStk[TASK_STK_SIZE];
+
+/* Parameters to pass to each task */
+char     TaskData[NO_TASKS];                    
+
+void Timer0_ISR(void);
+void Inval_ISR(void);
+void C_IRQHandler(void);
+void Task (void *data);
+void TaskStart (void *data);
+
+void (*irqtbl[32])(void)={
+	Inval_ISR,	//	EINT0
+	Inval_ISR,	//	EINT1
+	Inval_ISR,	//	EINT2
+	Inval_ISR,	//	EINT3
+	Inval_ISR,	//	EINT4_7
+	Inval_ISR,	//	EINT8_23
+	Inval_ISR,	//	pISR_CAM
+	Inval_ISR,	//	BAT_FLT	      
+	Inval_ISR,	//	TICK	      
+	Inval_ISR,	//	WDT_AC97T_AC97
+	Timer0_ISR,	//	TIMER0	      
+	Inval_ISR,	//	TIMER1	      
+	Inval_ISR,	//	TIMER2	      
+	Inval_ISR,	//	TIMER3	      
+	Inval_ISR,	//	TIMER4	      
+	Inval_ISR,	//      UART2	      
+    Inval_ISR,      //      LCD	      
+	Inval_ISR,      //      DMA0	      
+	Inval_ISR,	//	DMA1	 														     
+	Inval_ISR,	//	DMA2	 														     
+	Inval_ISR,	//	DMA3	 														     
+	Inval_ISR,	//	SDI	 														     
+	Inval_ISR,	//	SPI0	 														     
+	Inval_ISR,	//	UART1	 														     // Added for 2440.
+	Inval_ISR,	//	NFCON	 														     
+	Inval_ISR,	//	USBD	 														     
+	Inval_ISR,	//	USBH	 														     
+	Inval_ISR,	//	IIC	 														     
+	Inval_ISR,	//	UART0	 																     
+	Inval_ISR,	//	SPI1	 																     
+	Inval_ISR,	//	RTC	 																     
+	Inval_ISR	//	ADC	 																     
+};
+
+#endif
