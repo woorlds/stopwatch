eff1222175da1c6d3eabd82e41cf06dd1b52a900
Hyunwoo Lee <woorlds@gmail.com>
2013-09-11 14:23:05 +0900

    Single task

--- SRC/app/stop-watch.c
+++ SRC/app/stop-watch.c
@@ -24,7 +24,7 @@ void Main()
 	clear_screen(GREEN);
 
 	Uart_Send_String("Stop Watch in uC/OS-II. Designed by Hyunwoo Lee\n");
-	Lcd_Printf(10*8,0,WHITE,GREEN,1,1,"Stop Watch. Designed by Hyunwoo Lee");  
+	Lcd_Printf(10*8,0,BLUE,GREEN,1,1,"Stop Watch. Designed by Hyunwoo Lee");  
 
 	Timer_Init();
 
@@ -61,6 +61,26 @@ void C_IRQHandler(void)
 
 void Task (void *data)
 {
+	int  hour=0, min=0 , sec=0 , mm=0;
+	
+	while(1)
+	{
+		Lcd_Printf(300, 30+20 ,BLUE,GREEN,1,1,"%02d:%02d:%02d:%02d",hour, min, sec, mm);
+
+		if( mm++ == 59 )
+		{
+			sec++; mm = 0 ;
+		}
+		if( sec == 59 )
+		{
+			min++; sec = 0 ;
+		}
+		if( min == 59 )
+		{
+			hour++; min = 0 ;
+		}
+
+	}
 }
 
 
@@ -74,7 +94,7 @@ void TaskStart (void *data)
 		Timer_Start(OS_TICKS_PER_SEC);
 		
         OSStatInit();                           /* Initialize uC/OS-II's statistics */
-		
+
 #if 0
         for (i = 0; i < NO_TASKS; i++)
         {                                       /* Create NO_TASKS identical tasks */
@@ -84,17 +104,19 @@ void TaskStart (void *data)
         }
 #endif
 
+    	OSTaskCreate(Task, (void *)0, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 1);
+
         Uart_Printf("<-POWER OFF TO QUIT->\n");
 
-		Lcd_Printf(5*8,16*16,WHITE,GREEN,1,1,"Task Count: ",OSTaskCtr);  
-		Lcd_Printf(25*8,16*16,WHITE,GREEN,1,1,"CPU Usage: ",OSCPUUsage);  
-		Lcd_Printf(40*8,16*16,WHITE,GREEN,1,1,"Context SW: ",OSCtxSwCtr);  
+		Lcd_Printf(5*8,16*16,BLUE,GREEN,1,1,"Task Count: ",OSTaskCtr);  
+		Lcd_Printf(25*8,16*16,BLUE,GREEN,1,1,"CPU Usage: ",OSCPUUsage);  
+		Lcd_Printf(40*8,16*16,BLUE,GREEN,1,1,"Context SW: ",OSCtxSwCtr);  
 
         while(1)
         {
- 				Lcd_Printf(16*8,16*16,WHITE,GREEN,1,1,"%-3d",OSTaskCtr);  
- 				Lcd_Printf(35*8,16*16,WHITE,GREEN,1,1,"%-3d",OSCPUUsage);  
- 				Lcd_Printf(51*8,16*16,WHITE,GREEN,1,1,"%-3d",OSCtxSwCtr);  
+ 				Lcd_Printf(16*8,16*16,BLUE,GREEN,1,1,"%-3d",OSTaskCtr);  
+ 				Lcd_Printf(35*8,16*16,BLUE,GREEN,1,1,"%-3d",OSCPUUsage);  
+ 				Lcd_Printf(51*8,16*16,BLUE,GREEN,1,1,"%-3d",OSCtxSwCtr);  
 							
                 OSCtxSwCtr = 0;
                 
